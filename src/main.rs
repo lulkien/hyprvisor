c927a03bf41cbe56ea4aad3272d0275458a120f1
lulkien <kien.luuhoang.arch@gmail.com>
2023-09-13 00:16:19 +0700

    Features completed

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,69 @@
-use hyprland::data::{Client, Clients, Monitors, Workspace, Workspaces};
-use hyprland::dispatch::*;
+use std::env;
+
+use hyprland::data::Workspaces;
 use hyprland::event_listener::EventListenerMutable as EventListener;
-use hyprland::keyword::*;
 use hyprland::prelude::*;
-use hyprland::shared::WorkspaceType;
+use hyprland::shared::{HyprError, WorkspaceId, WorkspaceType};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct WorkspaceInfo {
+    id: WorkspaceId,
+    active: bool,
+    occupied: bool,
+}
 
 fn main() -> hyprland::Result<()> {
-    // let monitors = Monitors::get()?;
-    // println!("monitors: {monitors:#?}");
-    //
-    // let workspaces = Workspaces::get()?;
-    // println!("workspaces: {workspaces:#?}");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        let error_message = match args.len() {
+            0 => "Need an argument: workspace, window",
+            1 => "Need an argument: workspace, window",
+            _ => "Too many arguments",
+        };
+        return Err(HyprError::NotOkDispatch(error_message.to_string()));
+    }
 
     // Print workspace change
     let mut event_listener = EventListener::new();
+    match args[1].as_str() {
+        "workspace" => {
+            event_listener.add_workspace_change_handler(|data, _| match data {
+                WorkspaceType::Regular(active_workspace) => {
+                    let workspaces = Workspaces::get().unwrap();
+                    let occupied_workspace: Vec<WorkspaceId> =
+                        workspaces.iter().map(|workspace| workspace.id).collect();
 
-    event_listener.add_workspace_change_handler(|id, _| match id {
-        WorkspaceType::Regular(workspace) => {
-            println!("Active workspace changed: {}", workspace);
+                    let workspaces_info: Vec<WorkspaceInfo> = (1..=10)
+                        .map(|index| WorkspaceInfo {
+                            id: index,
+                            active: index.to_string() == active_workspace,
+                            occupied: occupied_workspace.contains(&index),
+                        })
+                        .collect();
+
+                    let workspace_json = serde_json::to_string(&workspaces_info).unwrap();
+                    println!("{}", workspace_json);
+                }
+                _ => {
+                    println!("Invalid");
+                }
+            });
+        }
+        "window" => {
+            event_listener.add_active_window_change_handler(|data, _| match data {
+                Some(window_data) => {
+                    println!("Window focus changed: {}", window_data.window_title);
+                }
+                None => println!("..."),
+            });
         }
         _ => {
-            println!("Invalid")
+            eprintln!("Error: Invalid argument '{}'", args[1]);
+            return Err(HyprError::NotOkDispatch("Invalid argument".to_string()));
         }
-    });
-
-    event_listener.add_active_window_change_handler(|data, _| match data {
-        Some(window_data) => {
-            println!("Window focus changed: {}", window_data.window_title)
-        }
-        None => println!("..."),
-    });
+    }
 
     event_listener.start_listener()
 }

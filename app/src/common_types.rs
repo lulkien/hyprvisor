9e22ce6df377d5658883c3310ee9190f79d6d245
lulkien <kien.luuhoang.arch@gmail.com>
2024-02-19 23:54:59 +0700

    Completed connection between client and server

--- app/src/common_types.rs
+++ app/src/common_types.rs
@@ -1,11 +1,36 @@
 use serde::{Deserialize, Serialize};
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter, Result};
 use tokio::net::UnixStream;
 
-#[derive(Debug, Deserialize, Serialize, PartialEq)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq, Hash)]
 pub enum SubscriptionID {
     Workspaces,
     Window,
 }
 
-pub type Subscriber = HashMap<SubscriptionID, HashSet<UnixStream>>;
+impl Display for SubscriptionID {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            SubscriptionID::Workspaces => write!(f, "Workspaces"),
+            SubscriptionID::Window => write!(f, "Window"),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ClientInfo {
+    pub process_id: u32,
+    pub subscription_id: SubscriptionID,
+}
+
+impl ClientInfo {
+    pub fn new(process_id: u32, subscription_id: SubscriptionID) -> Self {
+        ClientInfo {
+            process_id,
+            subscription_id,
+        }
+    }
+}
+
+pub type Subscriber = HashMap<SubscriptionID, HashMap<u32, UnixStream>>;

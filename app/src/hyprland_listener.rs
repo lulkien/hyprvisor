35ecc355c0635a64f822cbac77946a5b46064c99
lulkien <kien.luuhoang.arch@gmail.com>
2024-02-22 07:26:22 +0700

    Send window info to client

--- app/src/hyprland_listener.rs
+++ app/src/hyprland_listener.rs
@@ -1,18 +1,18 @@
 use crate::{
     client,
-    common_types::{HyprEvent, HyprSocketType, HyprWinInfo, Subscriber},
+    common_types::{HyprEvent, HyprSocketType, HyprWinInfo, Subscriber, SubscriptionID},
     opts::CommandOpts,
     utils,
 };
 use std::sync::Arc;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
-    net::UnixStream,
     sync::Mutex,
 };
 
 pub async fn start_hyprland_listener(subscribers: Arc<Mutex<Subscriber>>) {
     let event_socket = utils::get_hyprland_socket(&HyprSocketType::Event);
+    let mut current_win_info = HyprWinInfo::default();
 
     log::info!("Start Hyprland event listener");
     let mut event_listener = utils::try_connect(&event_socket, 1, 500).await.unwrap();
@@ -23,6 +23,10 @@ pub async fn start_hyprland_listener(subscribers: Arc<Mutex<Subscriber>>) {
             Ok(bytes) if bytes > 0 => {
                 let events = parse_events(&buffer[..bytes]);
                 log::info!("{:?}", events);
+                if events.contains(&HyprEvent::WindowChanged) {
+                    let subscribers_ref = Arc::clone(&subscribers);
+                    handle_window_changed_event(&mut current_win_info, subscribers_ref).await;
+                }
             }
 
             Ok(_) | Err(_) => {
@@ -55,4 +59,65 @@ fn parse_events(buffer: &[u8]) -> Vec<HyprEvent> {
         .collect();
     evt_list.dedup();
     evt_list
+}
+
+async fn get_hypr_active_window() -> HyprWinInfo {
+    let mut win_info = HyprWinInfo::default();
+    let cmd_sock = utils::get_hyprland_socket(&HyprSocketType::Command);
+    let raw_response = utils::write_to_socket(&cmd_sock, "activewindow", 1, 250)
+        .await
+        .unwrap();
+
+    let processed_data: Vec<String> = raw_response
+        .split('\n')
+        .map(|s| s.trim().to_string())
+        .filter(|s| s.starts_with("class: ") || s.starts_with("title: "))
+        .collect();
+
+    for d in processed_data {
+        if d.starts_with("class: ") {
+            win_info.class = d.strip_prefix("class: ").unwrap().to_string();
+        } else if d.starts_with("title: ") {
+            win_info.title = d.strip_prefix("title: ").unwrap().to_string();
+        } else {
+            // do nothing
+        }
+    }
+
+    win_info
+}
+
+async fn handle_window_changed_event(
+    current_win_info: &mut HyprWinInfo,
+    subscribers: Arc<Mutex<Subscriber>>,
+) {
+    let mut subscribers = subscribers.lock().await;
+    let win_subscribers = match subscribers.get_mut(&SubscriptionID::Window) {
+        Some(subs) if !subs.is_empty() => subs,
+        Some(_) | None => {
+            log::info!("No subscribers");
+            return;
+        }
+    };
+
+    let new_win_info = get_hypr_active_window().await;
+    if *current_win_info == new_win_info {
+        return;
+    }
+
+    *current_win_info = new_win_info.clone();
+
+    let mut disconnected_pid = Vec::new();
+    let window_json = serde_json::to_string(current_win_info).unwrap();
+
+    for (pid, stream) in win_subscribers.iter_mut() {
+        if stream.write_all(window_json.as_bytes()).await.is_err() {
+            println!("Client {} is disconnected. Remove later", pid);
+            disconnected_pid.push(*pid);
+        }
+    }
+
+    for pid in disconnected_pid {
+        win_subscribers.remove(&pid);
+    }
 }

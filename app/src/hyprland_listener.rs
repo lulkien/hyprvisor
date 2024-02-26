35416490e926e4782719e18bafaf841b43007f29
lulkien <kien.luuhoang.arch@gmail.com>
2024-02-27 02:23:27 +0700

    Done, need handle somehting on client side

--- app/src/hyprland_listener.rs
+++ app/src/hyprland_listener.rs
@@ -1,11 +1,11 @@
-use crate::{client, common_types::Subscriber, error::HyprvisorResult, opts::CommandOpts, utils};
+use crate::{common_types::Subscriber, error::HyprvisorResult, utils};
 use std::sync::Arc;
 use tokio::{io::AsyncReadExt, sync::Mutex};
 
 pub mod types;
+pub mod window;
+pub mod workspaces;
 use types::{HyprEvent, HyprSocketType, HyprWinInfo, HyprWorkspaceInfo};
-mod window;
-mod workspaces;
 
 pub async fn start_hyprland_listener(subscribers: Arc<Mutex<Subscriber>>) -> HyprvisorResult<()> {
     let event_socket = utils::get_hyprland_socket(&HyprSocketType::Event);
@@ -13,7 +13,13 @@ pub async fn start_hyprland_listener(subscribers: Arc<Mutex<Subscriber>>) -> Hyp
     let mut current_ws_info: Vec<HyprWorkspaceInfo> = Vec::new();
 
     log::info!("Start Hyprland event listener");
-    let mut event_listener = utils::try_connect(&event_socket, 1, 500).await.unwrap();
+    let mut event_listener = match utils::try_connect(&event_socket, 1, 500).await {
+        Ok(stream) => stream,
+        Err(e) => {
+            log::error!("{e}");
+            std::process::exit(1);
+        }
+    };
     let mut buffer: [u8; 8192] = [0; 8192];
 
     loop {
@@ -22,19 +28,18 @@ pub async fn start_hyprland_listener(subscribers: Arc<Mutex<Subscriber>>) -> Hyp
                 let events = parse_events(&buffer[..bytes]);
                 log::info!("{:?}", events);
                 if events.contains(&HyprEvent::WindowChanged) {
-                    window::broadcast_info(&mut current_win_info, subscribers.clone()).await?;
-                    workspaces::broadcast_info(&mut current_ws_info, subscribers.clone()).await?;
+                    send_window_info(&mut current_win_info, subscribers.clone()).await;
+                    send_workspace_info(&mut current_ws_info, subscribers.clone()).await;
                 } else if events.contains(&HyprEvent::WorkspaceCreated)
                     || events.contains(&HyprEvent::WorkspaceDestroyed)
                 {
-                    workspaces::broadcast_info(&mut current_ws_info, subscribers.clone()).await?;
+                    send_workspace_info(&mut current_ws_info, subscribers.clone()).await;
                 }
             }
 
             Ok(_) | Err(_) => {
                 log::error!("Connection closed from Hyprland event socket");
-                client::send_server_command(&utils::get_socket_path(), &CommandOpts::Kill, 1)
-                    .await?;
+                std::process::exit(1);
             }
         }
     }
@@ -62,4 +67,19 @@ fn parse_events(buffer: &[u8]) -> Vec<HyprEvent> {
         .collect();
     evt_list.dedup();
     evt_list
+}
+
+async fn send_window_info(current_info: &mut HyprWinInfo, subscribers: Arc<Mutex<Subscriber>>) {
+    if let Err(e) = window::broadcast_info(current_info, subscribers.clone()).await {
+        log::info!("{e}");
+    }
+}
+
+async fn send_workspace_info(
+    current_info: &mut Vec<HyprWorkspaceInfo>,
+    subscribers: Arc<Mutex<Subscriber>>,
+) {
+    if let Err(e) = workspaces::broadcast_info(current_info, subscribers.clone()).await {
+        log::info!("{e}");
+    }
 }
